875592011efc3a5d7c93d2596795af41d449b4ab
HV220 <diagenin@gmail.com>
2024-01-27 21:06:09 +0300

    final

--- app/src/main/java/com/example/muviesmobileapp/views/MainActivity.java
+++ app/src/main/java/com/example/muviesmobileapp/views/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.muviesmobileapp.views;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -7,6 +8,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 
@@ -27,11 +30,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = FavoriteMovieActivity.createIntent(MainActivity.this);
-        startActivity(intent);
-
         this.init();
-
 
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
 
@@ -62,5 +61,20 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(moviesAdapter);
 
         mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.itemFavorite) {
+            Intent intent = FavoriteMovieActivity.createIntent(MainActivity.this);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
